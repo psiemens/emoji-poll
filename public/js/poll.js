cd304f4b367749f5df6353555d1c5d700fa9a9c7
Peter Siemens <peterjsiemens@gmail.com>
2016-11-06 10:30:15 -0800

    Show number of results

--- public/js/poll.js
+++ public/js/poll.js
@@ -10,33 +10,41 @@ socket.emit('subscribe', { slug: slug });
 
 socket.on('update', renderChart);
 
-var chart = d3.select('.poll-results-chart');
+var chart = d3.select('.poll-results-bars');
 var x = d3.scaleLinear().range([10, 100]);
 
 function renderChart(data) {
 
   x.domain([0, d3.max(data)]);
 
-  var bars = chart.selectAll('.poll-results-chart-bar')
+  var totalResponses = d3.sum(data);
+
+  var bars = chart.selectAll('.poll-results-bar')
     .data(data);
 
   // Enter
   bars.enter().append('div')
-    .attr('class', 'poll-results-chart-bar')
+    .attr('class', 'poll-results-bar')
     .style('width', function(d) { return x(d) + '%'; })
     .append('div')
       .append('div')
-        .attr('class', 'poll-results-chart-bar-value')
-        .html(function(d) {return d; });
-
-  // Exit
-  //bars.exit().remove();
+        .attr('class', 'poll-results-bar-value')
+        .html(function(d) { return d; });
 
   // Update
   bars
-    .style('width', function(d) { return x(d) + '%'; });
-
-  bars
-    .select('.poll-results-chart-bar-value')
+    .style('width', function(d) { return x(d) + '%'; })
+    .select('.poll-results-bar-value')
       .html(function(d) {return d; });
+
+  // Update results count
+  d3.select('.poll-results-count')
+    .html(totalResponses + ' responses');
+
+  // Show results
+  if (totalResponses) {
+    d3.select('.poll-results')
+      .style('display', 'block');
+  }
+
 }
