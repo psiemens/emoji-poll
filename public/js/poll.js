fd2a67cddbc47f6c45072296f016876d9cef1b64
Peter Siemens <peterjsiemens@gmail.com>
2016-11-05 17:55:29 -0700

    log updates on client

--- public/js/poll.js
+++ public/js/poll.js
@@ -2,3 +2,7 @@ var socket = io.connect('http://emoji-poll.herokuapp.com/');
 
 var slug = $('.js-poll').data('slug');
 socket.emit('subscribe', { slug: slug });
+
+socket.on('update', function(data) {
+  console.log(data);
+})
