ef51c2d96aa114b53cab7df02d77980202530dbd
Peter Siemens <peterjsiemens@gmail.com>
2016-11-06 14:14:07 -0800

    Remove emoji-map.js to map.js

--- public/js/map.js
+++ public/js/map.js
@@ -2,30 +2,31 @@ var map = L.Mapzen.map('map', {
   'scene': '/emoji-map.yaml'
 })
 
-
 map.setView([37.7749, -122.4194], 4);
 
-
 var citiesLayer;
+var statesLayer;
 
 var poll = $('.js-poll');
 
 var slug = poll.data('slug');
 var options = poll.data('options');
 
-d3.json('/api/polls/fave-fruit/map', function(geojsonFeature) {
-  citiesLayer = L.geoJSON(
+d3.json('/api/polls/fave-fruit/map', function(data) {
+  citiesLayer = renderLayer(data.cities);
+  statesLayer = renderLayer(data.states);
+
+  updateLayers();
+});
+
+function renderLayer(geojsonFeature) {
+  return L.geoJSON(
     geojsonFeature,
     {
       pointToLayer: function (feature, latlng) {
-        console.log(feature);
-        console.log(latlng);
         var responses = feature.properties.responses;
 
-        console.log(responses);
-
         if (responses) {
-          console.log('rendering');
           var option = options[responses.indexOf(d3.max(responses))];
           var emojiIcon = L.divIcon({className: 'map-emoji', html: option.emoji});
           return L.marker(latlng, {icon: emojiIcon});
@@ -33,24 +34,34 @@ d3.json('/api/polls/fave-fruit/map', function(geojsonFeature) {
       }
     }
   );
-
-  updateLayers();
-});
+}
 
 function updateLayers() {
 
-  if (!citiesLayer) {
+  if (!citiesLayer || !statesLayer) {
     return;
   }
 
   var zoomBreakpoint = 6;
 
-  if (map.getZoom() > zoomBreakpoint && !map.hasLayer(citiesLayer)) {
-    map.addLayer(citiesLayer);
+  if (map.getZoom() > zoomBreakpoint) {
+    if (!map.hasLayer(citiesLayer)) {
+      map.addLayer(citiesLayer);
+    }
+
+    if (map.hasLayer(statesLayer)) {
+      map.removeLayer(statesLayer);
+    }
   }
 
-  if (map.getZoom() < zoomBreakpoint && map.hasLayer(citiesLayer)) {
-    map.removeLayer(citiesLayer);
+  if (map.getZoom() < zoomBreakpoint) {
+    if (map.hasLayer(citiesLayer)) {
+      map.removeLayer(citiesLayer);
+    }
+
+    if (!map.hasLayer(statesLayer)) {
+      map.addLayer(statesLayer);
+    }
   }
 }
 
